9266b87c46f510d2f8ae5a70d3e020cf562d930e
Aarti <webdevgenius2014@gmail.com>
2023-06-04 14:43:07 +0530

    apple login popup

--- src/app/form-components/apple-login.tsx
+++ src/app/form-components/apple-login.tsx
@@ -1,48 +1,49 @@
 import styles from '../../styles/styles.module.scss'
 import Image from 'next/image'
 import AuthService from '../../services/auth.service'
-import AppleLogin from 'react-apple-login';
+import { ButtonApple } from '@nixjs23n6/next-apple-login';
 import { useRouter } from 'next/navigation';
 
 const AppleLoginButton= () => {
     const router = useRouter();
     const clientId = process.env.NEXT_PUBLIC_APPLE_CLIENT_ID;
-    console.log(clientId);
     const redirectUrl = process.env.NEXT_PUBLIC_APPLE_REDIRECT_URL;
-    console.log(redirectUrl);
-
-    const appleResponse = async(response:any) => {
-        console.log(response)
-        if (!response.error) {
-            console.log(response)
-            let params = {
-                "type" : "apple",
-                "name" : response?.data?.name,
-                "email" : response.data.email
-            }
-            await AuthService.socialLogin(params).then((data)=>{      
-                if(data){
-                    console.log("get token for verified emil from backend api");  
-                    console.log(data);
-                    sessionStorage.setItem("auth_token", data.data.token);
-                    setTimeout(() => {
-                        router.push('/');
-                    }, 3000); 
-                }
-            })
-        }
+    const responseApple = (response:any) => {
+        console.log(response, response.authorization?.id_token)
     }
 
+    // const appleResponse = async(response:any) => {
+    //     console.log(response)
+    //     if (!response.error) {
+    //         console.log(response)
+    //         let params = {
+    //             "type" : "apple",
+    //             "name" : response?.data?.name,
+    //             "email" : response.data.email
+    //         }
+    //         await AuthService.socialLogin(params).then((data)=>{      
+    //             if(data){
+    //                 console.log("get token for verified emil from backend api");  
+    //                 console.log(data);
+    //                 sessionStorage.setItem("auth_token", data.data.token);
+    //                 setTimeout(() => {
+    //                     router.push('/');
+    //                 }, 3000); 
+    //             }
+    //         })
+    //     }
+    // }
+
     return( 
-        <AppleLogin
-        clientId={clientId}
-        redirectURI={redirectUrl}
-        usePopup={true}
-        callback={appleResponse} // Catch the response
-        scope="email name"
-        responseMode="query"
-        render={renderProps => ( 
-            <button type='button' className={`d-block ms-auto me-auto ${styles.btn} ${styles.other_login}`}>
+        <ButtonApple
+            clientId={clientId}
+            redirectURI={redirectUrl}
+            responseType="id_token"
+            responseMode="query"
+            callback={responseApple}
+            className={`d-block ms-auto me-auto ${styles.btn} ${styles.other_login}`}
+        >
+            <>
                 <Image
                 src="/apple.svg"
                 width={20}
@@ -50,9 +51,8 @@ const AppleLoginButton= () => {
                 alt="apple"                
                 />           
                 <span className='ms-3'>Login With Apple</span>
-            </button>
-        )}
-        />
+            </>
+        </ButtonApple>
     );
 }
 export default AppleLoginButton;
